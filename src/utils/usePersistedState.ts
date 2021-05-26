ea385318893ea7545e67972bd4cd98d6f23a05b1
jonatasvenancio167 <jvenancio167@hotmail.com>
2021-05-26 13:30:45 -0300

    Criacao do localStorage

--- src/utils/usePersistedState.ts
+++ src/utils/usePersistedState.ts
@@ -5,21 +5,22 @@ type Response<T> = [
     Dispatch<SetStateAction<T>>,
 ]
 
-export default function usePersistedState<T>(key: string, initialState: T): Response<T>{
-    // const [state, setState] = useState(() => {
-    //     const storageValue = localStorage.getItem(key)
-        
-    //     if (storageValue) {
-    //       return JSON.parse(storageValue)
-    //     } else {
-    //       return initialState
-    //     }
-    // })
-
+export default function usePersistedState<T>(key: string, initialState: any): Response<T>{
+    
     const [state, setState] = useState(initialState)
 
     useEffect(() => {
-        localStorage.setItem(key, JSON.stringify(state));
+        const storageValue = window.localStorage.getItem(key)
+        
+        if (storageValue){
+            return  setState(JSON.parse(storageValue)) 
+          } else {
+            return initialState
+          }
+    }, [key])
+
+    useEffect(() => {
+        window.localStorage.setItem(key, JSON.stringify(state));
     }, [key, state])
 
   return [state, setState];
